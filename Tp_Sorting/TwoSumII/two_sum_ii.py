5c95f94cc668934726698cdc263d6ac6c3611e5f
Kareem Salem <kareem.salem@blackcape.io>
2025-07-31 07:39:34 -0400

    Added two sum II (two sum solved with two pointers)

---
+++ Tp_Sorting/TwoSumII/two_sum_ii.py
@@ -0,0 +1,14 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+# Return the indices of the two numbers as an array [index1, index2] of length 2.
+# You must use exactly one solution, and you may not use the same element twice.
+# Your solution must use only constant extra space (no hashmaps).
+
+def two_sum_ii(arr, target):
+    return
+
+if __name__ == '__main__':
+    print(two_sum_ii([1, 2, 3, 4, 5, 6], 8))
+    print(two_sum_ii([1], 7))
+    print(two_sum_ii([], 7))
+    print(two_sum_ii([5, 6], 7))
+    print(two_sum_ii([1, 2, 3, 4, 6, 7], 7))

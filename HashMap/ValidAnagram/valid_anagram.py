386db2e981d23bb05d425a23745b4960fd177e9a
Kareem Salem <kareem.salem@blackcape.io>
2025-07-31 06:55:51 -0400

    Added roadmap

--- HashMap/ValidAnagram/valid_anagram.py
+++ HashMap/ValidAnagram/valid_anagram.py
@@ -2,4 +2,3 @@
 # Given two strings s and t, return True if t is an anagram of s, and False otherwise.
 # An anagram is a word formed by rearranging the letters of another word.
 # Example: "listen" and "silent" are anagrams.
-

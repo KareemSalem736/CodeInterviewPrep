c144aff6c9dd2a1402ab8c07bf01d2c64f859e8c
Kareem Salem <kareem.salem@blackcape.io>
2025-08-01 08:09:13 -0400

    Added three sum to tp and sorting

--- Tp_Sorting/ThreeSum/three_sum.py
+++ Tp_Sorting/ThreeSum/three_sum.py
@@ -4,14 +4,15 @@
 # Return the triplets in any order, but no duplicates allowed.
 
 def three_sum(arr):
-    return
+
+    return 
 
 if __name__ == '__main__':
-    print(three_sum([-1, 0, 1, 2, -1, -4]))# Expected: [[-1, -1, 2], [-1, 0, 1]]
-    print(three_sum([0, 0, 0, 0]))# Expected: [[0, 0, 0]]
-    print(three_sum([-2, 0, 1, 1, 2]))# Expected: [[-2, 1, 1], [-2, 0, 2]]
     print(three_sum([]))# Expected: []
     print(three_sum([0]))# Expected: []
     print(three_sum([0, 1]))# Expected: []
+    print(three_sum([3, -2, 1, 0]))# Expected: []
     print(three_sum([-4, -1, -1, 0, 1, 2]))# Expected: [[-1, -1, 2], [-1, 0, 1]]
-    print(three_sum([3, -2, 1, 0]))# Expected: [[-2, 1, 1], [-2, 0, 2]] (or variations)
+    print(three_sum([-2, 0, 1, 1, 2]))# Expected: [[-2, 1, 1], [-2, 0, 2]]
+    print(three_sum([0, 0, 0, 0]))# Expected: [[0, 0, 0]]
+    print(three_sum([-1, 0, 1, 2, -1, -4]))# Expected: [[-1, -1, 2], [-1, 0, 1]]

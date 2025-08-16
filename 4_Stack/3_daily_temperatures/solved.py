5856eeb077310bbb2b66dff629c5c9757867e75a
Kareem Salem <kareem.salem@blackcape.io>
2025-08-16 09:02:23 -0400

    Completed Stack section

--- 4_Stack/3_daily_temperatures/solved.py
+++ 4_Stack/3_daily_temperatures/solved.py
@@ -1,9 +1,24 @@
 # Given a list of daily temperatures, return a list that tells for each day how many days until a warmer temperature.
 
-def daily_temperatures(temps):
-    pass
+class DailyTemperatures:
+    def __init__(self, arr) -> None:
+        self.arr = arr
+
+    def daily_temperatures(self) -> list[int]:
+        n = len(self.arr)
+        result = [0] * n
+        stack = []  # holds indices of days
+
+        for i, temp in enumerate(self.arr):
+            # If current day is warmer than the day on top of stack
+            while stack and temp > self.arr[stack[-1]]:
+                prev_day = stack.pop()
+                result[prev_day] = i - prev_day
+            stack.append(i)
+
+        return result
 
 if __name__ == "__main__":
-    print(daily_temperatures([73,74,75,71,69,72,76,73]))  # [1,1,4,2,1,1,0,0]
-    print(daily_temperatures([30,40,50,60]))               # [1,1,1,0]
-    print(daily_temperatures([30,60,90]))                  # [1,1,0]
+    print(DailyTemperatures([73,74,75,71,69,72,76,73]).daily_temperatures())# [1,1,4,2,1,1,0,0]
+    print(DailyTemperatures([30,40,50,60]).daily_temperatures())# [1,1,1,0]
+    print(DailyTemperatures([30,60,90]).daily_temperatures()) # [1,1,0]         
--- 4_Stack/3_daily_temperatures/unsolved.py
+++ 4_Stack/3_daily_temperatures/unsolved.py
@@ -1,9 +1,7 @@
 # Given a list of daily temperatures, return a list that tells for each day how many days until a warmer temperature.
 
-def daily_temperatures(temps):
-    pass
 
 if __name__ == "__main__":
-    print(daily_temperatures([73,74,75,71,69,72,76,73]))  # [1,1,4,2,1,1,0,0]
-    print(daily_temperatures([30,40,50,60]))               # [1,1,1,0]
-    print(daily_temperatures([30,60,90]))                  # [1,1,0]
+    print(DailyTemperatures([73,74,75,71,69,72,76,73]).daily_temperatures())# [1,1,4,2,1,1,0,0]
+    print(DailyTemperatures([30,40,50,60]).daily_temperatures())# [1,1,1,0]
+    print(DailyTemperatures([30,60,90]).daily_temperatures()) # [1,1,0]         
